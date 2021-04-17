24e413a612fec3dcbd596ca48a924e2d322a4661
davidgq10 <davidgq10@hotmail.com>
2021-04-17 16:44:36 +0000

    Primera entrega

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -42,27 +42,35 @@ export function Home(props) {
 				<Container className="justify-content-between">
 					<div className="col-2 py-3 text-center">
 						<h1 className="display-1">
-							<i
-								className="bi-alarm"
-								style="font-size: 2rem; color: cornflowerblue;"></i>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="150"
+								height="150"
+								fill="currentColor"
+								className="bi bi-clock"
+								viewBox="0 0 16 16">
+								style={}
+								<path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+								<path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+							</svg>
 						</h1>
 					</div>
-					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-warning rounded">
+					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-light rounded">
 						<h1 className="display-1">{sixNumber}</h1>
 					</div>
-					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-warning rounded">
+					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-light rounded">
 						<h1 className="display-1">{fiveNumber}</h1>
 					</div>
-					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-warning rounded">
+					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-light rounded">
 						<h1 className="display-1">{fourNumber}</h1>
 					</div>
-					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-warning rounded">
+					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-light rounded">
 						<h1 className="display-1">{threeNumber}</h1>
 					</div>
-					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-warning rounded">
+					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-light rounded">
 						<h1 className="display-1">{twoNumber}</h1>
 					</div>
-					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-warning rounded">
+					<div className="col-1 bg-secondary py-3 text-center fw-bolder border border-light rounded">
 						<h1 className="display-1">{oneNumber}</h1>
 					</div>
 				</Container>
